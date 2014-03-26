ae4330770f2a2d791219d7dd45539ec1696aeaec
carolinavargas <carito2770@hotmail.com>
2014-03-26 15:01:33 -0500

    Update views.py

--- equipos/views.py
+++ equipos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from models import Continente, Jugador
+
 
 # Create your views here.
+
+class ListarContinentes(ListView):
+    model = Continente
+    template_name = 'lista_continentes.html'
+
+
+class ListarJugadores(ListView):
+    model = Jugador
+    template_name = 'lista_jugadores.html'
