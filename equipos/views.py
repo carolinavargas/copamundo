4eac7296ed05f591e90ea8ccd6732a21e7752dc0
Jhoana <carito2770@hotmail.com>
2014-04-03 19:25:34 -0500

    Clase Final

--- equipos/views.py
+++ equipos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import Http404
+from models import Continente, Jugador, Equipo
+
 
 # Create your views here.
+
+
+class ListarContinentes(ListView):
+    model = Continente
+    template_name = 'lista_continentes.html'
+
+class ListarEquipos(ListView):
+    model = Equipo
+    template_name = 'lista_equipos.html'
+
+class ListarJugadores(ListView):
+    model = Jugador
+    template_name = 'listar_jugadores.html'
+
+
+
+def detalleJugador(request, jugador_id):
+    try:
+        jugador = Jugador.objects.get(pk=jugador_id)
+    except Jugador.DoesNotExist:
+        raise Http404
+    return render(request, 'player.html', {'jugador': jugador})
